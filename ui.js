5a051ae8d03268dfa63b213fd5cc9bdc1a41d417
Michiel van der Blonk <blonkm@gmail.com>
2023-04-19 20:49:18 -0400

    closes #1 webworkers keep running after clicking STOP
    Solution found by reading i-programmer article on web workers: https://i-programmer.info/programming/javascript/16239-javascript-async-basic-worker.html
    
    The solution is to use a setTimeout inside the web worker so it has time to respond to the stop event.

--- ui.js
+++ ui.js
@@ -5,78 +5,77 @@ import { TableExport } from './classes/tableExport.js'
 
 $(function () {
 
-  const TXTSTOP = "STOPPEN"
+   const TXTSTOP = "STOPPEN"
 
-  let config = {
-    "iterations": 1000
-    , "size": 20
-    , "crossover": 0.1
-    , "mutation": 0.5
-    , "skip": 10
-    , "webWorkers": true
-  };
+   let config = {
+      "iterations": 1000
+      , "size": 20
+      , "crossover": 0.1
+      , "mutation": 0.5
+      , "skip": 10
+      , "webWorkers": true
+   };
 
-  /* assert: numSlots * slotSize >= numExams */
-  let userData = {
-    "solution": []
-    , "conflicts": []
-    , "numSlots": 8
-    , "slotSize": 50
-    , "exams": []
-    , "done": false
-  };
+   /* assert: numSlots * slotSize >= numExams */
+   let userData = {
+      "solution": []
+      , "conflicts": []
+      , "numSlots": 8
+      , "slotSize": 50
+      , "exams": []
+      , "done": false
+   };
 
-  let genetic
-  let scheduler
-  
-  $('#run').on('click', function () {
-    if ($(this).text() == TXTSTOP) {
-      scheduler.finalize(genetic)
-      document.dispatchEvent(new Event('Finished'))
-    }
-    else {
-      if ($('#inschrijvingen').val()=='') {
-        alert('Geen examens gevonden. Kopieer eerst de lijst met examens')
-        return false;
+   let genetic
+   let scheduler
+
+   $('#run').on('click', function () {
+      if ($(this).text() == TXTSTOP) {
+         scheduler.finalize(genetic)
       }
-      config.size = Number($("input[name='populationSize']").val())
-      config.mutation = Number($("input[name='mutationProbability']").val())/100
-      config.iterations = Number($("input[name='generations']").val())
+      else {
+         if ($('#inschrijvingen').val() == '') {
+            alert('Geen examens gevonden. Kopieer eerst de lijst met examens')
+            return false;
+         }
+         config.size = Number($("input[name='populationSize']").val())
+         config.mutation = Number($("input[name='mutationProbability']").val()) / 100
+         config.iterations = Number($("input[name='generations']").val())
 
-      let days = Number($('input[name = days]').val())
-      let examsPerDay = Number($('input[name = examsPerDay]').val())
-      let maxSlots = days * examsPerDay
-      userData.numSlots = maxSlots
-      $(this).html(TXTSTOP)
-      $(this).toggleClass("active")
-      scheduler = new Scheduler(config, userData)
-      genetic = scheduler.schedule()
-    }
-  })
+         let days = Number($('input[name = days]').val())
+         let examsPerDay = Number($('input[name = examsPerDay]').val())
+         let maxSlots = days * examsPerDay
+         userData.numSlots = maxSlots
+         $(this).html(TXTSTOP)
+         $(this).toggleClass("active")
+         scheduler = new Scheduler(config, userData)
+         genetic = scheduler.schedule()
+      }
+   })
 
-  $('#showHelp').on('click', function () {
-     $('.intro').toggleClass('hide')
-  })
+   $('#showHelp').on('click', function () {
+      $('.intro').toggleClass('hide')
+   })
 
-  $('#showTechnical').on('click', function () {
-     $('.technical').toggleClass('hide')
-  })
-  
-  $('#downloadSolution').on('click', function () {
-    let objExport = new TableExport('.solution table.exams')
-    objExport.toCSV().download('solution', true) // with timestamp, e.g. solution-1234.csv
-  })
+   $('#showTechnical').on('click', function () {
+      $('.technical').toggleClass('hide')
+   })
 
-  $('#downloadStudentList').on('click', function () {
-    let objExport = new TableExport('#studentSchedule')
-    objExport.toCSV().download('studentSchedule', true) // with timestamp, e.g. studentSchedule-1234.csv
-  })
+   $('#downloadSolution').on('click', function () {
+      let objExport = new TableExport('.solution table.exams')
+      objExport.toCSV().download('solution', true) // with timestamp, e.g. solution-1234.csv
+   })
 
-    // Add an event listener.
-  $(document).on('Finished', function(e, opts) {
-    $('#run').html(Scheduler.TXTGO)
-    $('#run').toggleClass("active")
-    console.log("finished")
-  });
+   $('#downloadStudentList').on('click', function () {
+      let objExport = new TableExport('#studentSchedule')
+      objExport.toCSV().download('studentSchedule', true) // with timestamp, e.g. studentSchedule-1234.csv
+   })
+
+   // Add an event listener.
+   $(document).on('Finished', function (e, opts) {
+      $('#run').html(Scheduler.TXTGO)
+      $('#run').toggleClass("active")
+      console.log("finished")
+   });
 
 })
